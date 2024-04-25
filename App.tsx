9473184cea88ec1449e3b9d8704f4fa1b9c3beba
MD <mdeanwill@gmail.com>
2024-04-25 13:31:52 -0700

    finished simple lab for Expo

--- App.tsx
+++ App.tsx
@@ -3,9 +3,15 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.back}>
+      <View style={styles.root}>
+        <View style={styles.container}>
+          <Text style={{ color: '#dc143c' }}>
+            #crushingit
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +19,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  root: {
+    flex: 1,
+    backgroundColor: '#dc143c',
+    padding: 20,
+  },
+  back: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    padding: 20,
   },
 });
